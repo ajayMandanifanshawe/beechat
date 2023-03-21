722f792e901ab58a3f8a5023c6b1254b42adf82d
surajgrgca <s_gurung168925@fanshaweonline.ca>
2023-03-20 21:03:51 -0400

    [#11] User can see the list of user they have chatted with On the home screen you can see the list of user you have already started engaged with. On the home screen it shows the other users Name, last message, timestamp and the total number of unread messages closes #11

--- BeeChat/View/HomeTableViewController.swift
+++ BeeChat/View/HomeTableViewController.swift
@@ -5,20 +5,72 @@
 //  Created by Nihas Yousuf on 2023-02-05.
 //
 
-import UIKit
 import FirebaseAuth
-class HomeTableViewController: UITableViewController {
+import UIKit
 
-  
+import FirebaseAuth
+import UIKit
+class HomeTableViewController: UITableViewController {
+    var allLatest: [RecentChat] = []
+    var filteredLatest: [RecentChat] = []
+    let searchContollers = UISearchController(searchResultsController: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        donwloadAlldata()
     }
     
+    private func donwloadAlldata() {
+        FirebaseLatestListener.shared.downloadAllLatestfromfirestore { latest in
+            if let latest = latest {
+                self.allLatest = latest
+               
+            } else {
+                self.allLatest = []
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+  
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        // #warning Incomplete implementation, return the number of rows
+        return  allLatest.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as! RecentChatTableViewCell
+        
+        cell.config(recentChat: allLatest[indexPath.row])
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let recent = allLatest[indexPath.row]
+        
+        FirebaseLatestListener.shared.clearunreadCounter(recent: recent)
+        gotochat(recent: recent)
+    }
+
+    func gotochat(recent: RecentChat) {
+        restartChat(chartRoomId: recent.chatRoomId, memeberIds: recent.memeberIds)
+        let privatChatView = MessageViewController(chatId: recent.chatRoomId, recepientId: recent.receiverId, recepientName: recent.receiverName)
+        privatChatView.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(privatChatView, animated: true)
+    }
+
+    @IBAction func logout(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            UserDefaults.standard.removeObject(forKey: ConstCurrentUser)
+            let story = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "welcome")
+            story.modalPresentationStyle = .fullScreen
+            present(story, animated: true)
+        } catch {}
     }
     
-   
-
+  
 }
